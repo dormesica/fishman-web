39bd484d88439a52be343053442f9d4878c24483
Dor Mesica <dormesica10@gmail.com>
2019-01-16 15:16:06 +0200

    Removed var from all files. Added .prettierrc for code formatting.

--- bin/server.js
+++ bin/server.js
@@ -9,49 +9,48 @@ const ss = require('socket.io-stream');
 const fishmanWeb = require('../lib');
 const path = require('path'); //used only for express to serve statics
 
-
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 server.listen(process.env.PORT || 8080);
 
-io.on('connection', function (socket) {
-    socket.on('fishmanRequest', function (request) {
+io.on('connection', function(socket) {
+    socket.on('fishmanRequest', function(request) {
         const options = {
             packageManager: request.pm,
             modules: request.modules,
             incDeps: request.incDeps,
             incDevDeps: request.incDevDeps,
             incTypes: request.incTypes,
-            basePath: "/",
+            basePath: '/',
         };
 
         const fileSystem = new memoryFileSystem();
 
         let finalDownload = {
             stream: ss.createStream(),
-            size: 0
+            size: 0,
         };
 
-        fishmanWeb.cloneModule(options, fileSystem, finalDownload, function (typeOfUpdate, content) {
+        fishmanWeb.cloneModule(options, fileSystem, finalDownload, function(typeOfUpdate, content) {
             switch (typeOfUpdate) {
-                case "downloadProgress":
+                case 'downloadProgress':
                     socket.emit(typeOfUpdate, content);
-                    break;          
-                case "regularUpdate":
+                    break;
+                case 'regularUpdate':
                     content.message = decodeURIComponent(content.message);
                     socket.emit(typeOfUpdate, content); //decode
                     break;
-                case "criticalError":
+                case 'criticalError':
                     content.message = decodeURIComponent(content.message);
-                    socket.emit(typeOfUpdate, content); //decode 
+                    socket.emit(typeOfUpdate, content); //decode
                     socket.disconnect();
                     break;
-                case "finalDownloadToClient":
+                case 'finalDownloadToClient':
                     socket.emit('finalDownloadToClientMD', finalDownload.size);
                     ss(socket).emit(typeOfUpdate, finalDownload.stream);
                     break;
                 default:
-                    console.log('this should never happen - typeOfUpdate ' + typeOfUpdate +' did not match!');      
+                    console.log('this should never happen - typeOfUpdate ' + typeOfUpdate + ' did not match!');
             }
         });
     });
